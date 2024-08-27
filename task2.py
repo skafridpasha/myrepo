9cd87fe515d5c373425e8526233a358332c55234
skafridpasha <skafridpasha444@gmail.com>
2024-08-27 10:58:57 +0530

    my first commit

---
+++ task2.py
@@ -0,0 +1,11 @@
+option=input("enter your option:")
+if option=='withdraw':
+    print('take ur amount')
+elif option=='pinchange':
+    print('pin change sucessful')
+elif option=='balance':
+    print('see ur blnc')
+elif option=='language':
+    print('select ur language')
+else:
+    print('thank you')     
